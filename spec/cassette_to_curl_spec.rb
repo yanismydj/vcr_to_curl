386f697cbb5de4b1609afac041d7379156cff5d8
Piotr Wasiak <chooosen@gmail.com>
2018-05-22 13:17:48 +0200

    Test cassette with passed query

--- spec/cassette_to_curl_spec.rb
+++ spec/cassette_to_curl_spec.rb
@@ -50,27 +50,45 @@ describe VcrToCurl::CassetteToCurl do
        expect(curl_command).to eq("curl -X POST -d 'foo=blah' -H 'Yan: isawesome' 'http://echo.httpkit.com/'")
       end
     end
+
+    describe 'request with query' do
+      let(:cassette) { 'fixtures/vcr_cassettes/sample_vcr_cassette_with_query.yml' }
+      let(:vcr_to_curl) { VcrToCurl::CassetteToCurl.new(cassette) }
+
+      it 'creates the correct curl command' do
+       curl_command = vcr_to_curl.curl_commands[0]
+       expect(curl_command).to eq("curl -X GET -H 'Accept-encoding: gzip;q=1.0,deflate;q=0.6,identity;q=0.3' -H 'Accept: */*' -H 'User-agent: Ruby' 'https://www.google.com/?give_a_bit=to_me'")
+      end
+    end
   end
 
+  # methods to create cassettes only
   describe 'create sample cassettes' do
-    it 'creates a new cassette' do
+    xit 'creates a new cassette' do
       VCR.use_cassette('sample vcr cassette') do
         uri = 'http://echo.httpkit.com'
         response = HTTParty.post(uri, body: { 'fizz' => 'buzz', 'foo' => 'bar' })
       end
     end
 
-    it 'creates a cassette with headers' do
+    xit 'creates a cassette with headers' do
       VCR.use_cassette('sample vcr cassette with headers') do
         uri = 'http://echo.httpkit.com'
         response = HTTParty.post(uri, headers: { 'Yan' => 'isawesome' })
       end
     end
 
-    it 'creates a cassette with headers and post data' do
+    xit 'creates a cassette with headers and post data' do
       VCR.use_cassette('sample vcr cassette with headers and post data') do
         uri = 'http://echo.httpkit.com'
         response = HTTParty.post(uri, headers: { 'Yan' => 'isawesome' }, body: { foo: "blah"})
+      end
+    end
+
+    xit 'creates a cassette with headers, params and post data' do
+      VCR.use_cassette('sample vcr cassette with query') do
+        uri = 'https://www.google.com' # service echo.httpkit.com does not work
+        response = HTTParty.get(uri, query: { give_a_bit: :to_me })
       end
     end
   end
